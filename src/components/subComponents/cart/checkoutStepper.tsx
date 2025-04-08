8ff896acc3ff597d634902663712e48386c0a42b
DanialWaris <56953791+DanialWaris@users.noreply.github.com>
2025-04-08 09:03:28 +0500

    changes 8

--- src/components/subComponents/cart/checkoutStepper.tsx
+++ src/components/subComponents/cart/checkoutStepper.tsx
@@ -12,13 +12,26 @@ export const CheckoutStepper: FC<CheckoutStepperProps> = ({
     step,
 }): JSX.Element => {
     const router = useRouter()
+
+    const nav1Route =
+        typeof window !== "undefined" &&
+        window.location.pathname.includes("/tricart-electronics")
+    const nav2Route = typeof window !== "undefined" &&
+        window.location.pathname.includes("/tricart-tools")
+    const nav3Route = typeof window !== "undefined" &&
+        window.location.pathname.includes("/tricart-home-decore")
+    const nav4Route = typeof window !== "undefined" &&
+        window.location.pathname.includes("/tricart-fashion")
+
+    const templateBasePath = nav1Route ? "/tricart-electronics" : nav2Route ? "/tricart-tools" : nav3Route ? "/tricart-home-decore" : nav4Route ? "/tricart-fashion" : "/"
+
     return (
         <div className=" flex items-center gap-x-3 sm:gap-x-5">
-            <button onClick={() => step > 1 && router.push("/cart")} className={`${step === 1 ? "text-zinc-700" : step > 1 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Shopping Cart</button>
+            <button onClick={() => step > 1 && router.push(templateBasePath + "/cart")} className={`${step === 1 ? "text-zinc-700" : step > 1 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Shopping Cart</button>
             <FontAwesomeIcon icon={faChevronRight} className="text-xs sm:text-sm md:text-base lg:text-lg" />
-            <button onClick={() => step > 2 && router.push("/checkout")} className={`${step === 2 ? "text-zinc-700" : step > 2 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Checkout</button>
+            <button onClick={() => step > 2 && router.push(templateBasePath + "/checkout")} className={`${step === 2 ? "text-zinc-700" : step > 2 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Checkout</button>
             <FontAwesomeIcon icon={faChevronRight} className="text-xs sm:text-sm md:text-base lg:text-lg" />
-            <button onClick={() => step > 3 && router.push("/order-completed")} className={`${step === 3 ? "text-zinc-700" : step > 3 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Order Complete</button>
+            <button onClick={() => step > 3 && router.push(templateBasePath + "/order-completed")} className={`${step === 3 ? "text-zinc-700" : step > 3 ? "text-black hover:text-zinc-600 cursor-pointer" : "cursor-not-allowed text-zinc-300"} fw_600 text-xs sm:text-sm md:text-base lg:text-lg`}>Order Complete</button>
         </div>
     )
 }
